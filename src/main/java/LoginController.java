5f948dc614c124e9d74a70fab02107718967c2ad
Emir Hakimi <memirhakimi29@gmail.com>
2022-07-20 02:57:36 +0800

    Update LoginController.java

--- src/main/java/LoginController.java
+++ src/main/java/LoginController.java
@@ -18,7 +18,7 @@ import khairat.dao.KariahDAO;
 /**
  * Servlet implementation class LoginController
  */
-@WebServlet("/LoginController")
+
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	HttpSession session;
